a21c0fffe7d3be3c725a84fb8fb6c18ea38f6706
Simon Mikkelsen <simonbodall@runbox.com>
2021-08-11 19:42:01 +0200

    Remove all plugins

--- Atreus.ino
+++ Atreus.ino
@@ -3,19 +3,6 @@
 #endif
 
 #include "Kaleidoscope.h"
-#include "Kaleidoscope-EEPROM-Settings.h"
-#include "Kaleidoscope-EEPROM-Keymap.h"
-#include "Kaleidoscope-FocusSerial.h"
-#include "Kaleidoscope-Macros.h"
-#include "Kaleidoscope-MouseKeys.h"
-#include "Kaleidoscope-OneShot.h"
-#include "Kaleidoscope-Qukeys.h"
-#include "Kaleidoscope-SpaceCadet.h"
-
-enum {
-	MACRO_QWERTY,
-	MACRO_VERSION_INFO
-};
 
 enum {
 	QWERTY
@@ -36,44 +23,8 @@ KEYMAPS(
 	)
 )
 
-KALEIDOSCOPE_INIT_PLUGINS(
-	EEPROMSettings,
-	EEPROMKeymap,
-	Focus,
-	FocusEEPROMCommand,
-	FocusSettingsCommand,
-	Qukeys,
-	SpaceCadet,
-	OneShot,
-	Macros,
-	MouseKeys
-);
-
-const macro_t *macroAction(uint8_t macro_id, KeyEvent &event) {
-	if (keyToggledOn(event.state)) {
-		switch (macro_id) {
-		case MACRO_QWERTY:
-			// This macro is currently unused, but is kept around for compatibility
-			// reasons. We used to use it in place of `MoveToLayer(QWERTY)`, but no
-			// longer do. We keep it so that if someone still has the old layout with
-			// the macro in EEPROM, it will keep working after a firmware update.
-			Layer.move(QWERTY);
-			break;
-		case MACRO_VERSION_INFO:
-			Macros.type(PSTR("Keyboardio Atreus - Kaleidoscope "));
-			Macros.type(PSTR(BUILD_INFORMATION));
-			break;
-		default:
-			break;
-		}
-	}
-	return MACRO_NONE;
-}
-
 void setup() {
 	Kaleidoscope.setup();
-	SpaceCadet.disable();
-	EEPROMKeymap.setup(10);
 }
 
 void loop() {
