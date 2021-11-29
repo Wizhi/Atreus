2b67476920a8fee851be976a98e22b4ed16fcbfe
Simon Mikkelsen <simonbodall@runbox.com>
2021-11-29 19:38:49 +0100

    Restore from backup

--- Atreus.ino
+++ Atreus.ino
@@ -10,16 +10,17 @@
 
 enum {
 	QWERTY,
-	NUMPAD
+	NUMPAD,
+	SYMBOL
 };
 
 KEYMAPS(
 	[QWERTY] = KEYMAP_STACKED
 	(
 		Key_Q,    Key_W, Key_E, Key_R,         Key_T,
-		Key_A,    Key_S, Key_D, LT(NUMPAD, F), Key_G,
-		SFT_T(Z), Key_X, Key_C, Key_V,         Key_B,     ___,
-		___,      ___,   ___,   Key_LeftGui,   Key_Space, Key_Enter,
+		Key_A,    Key_S, Key_D, LT(NUMPAD, F), LT(SYMBOL, G),
+		SFT_T(Z), Key_X, Key_C, Key_V,         Key_B,         ___,
+		___,      ___,   ___,   Key_LeftGui,   Key_Space,     SFT_T(Enter),
 
 		         Key_Y,         Key_U, Key_I,     Key_O,      Key_P,
 		         Key_H,         Key_J, Key_K,     Key_L,      Key_Semicolon,
@@ -27,6 +28,18 @@ KEYMAPS(
 		Key_Tab, Key_Backspace,  ___,  ___,       ___,        ___
 	),
 	[NUMPAD] = KEYMAP_STACKED
+	(
+		___, ___, ___, ___, ___,
+		___, ___, ___, ___, ___,
+		___, ___, ___, ___, ___, ___,
+		___, ___, ___, ___, ___, ___,
+
+		     Key_Period, Key_1,     Key_2, Key_3, Key_Equals,
+		     Key_0,      Key_4,     Key_5, Key_6, Key_Plus,
+		___, Key_Star,   Key_7,     Key_8, Key_9, Key_Minus,
+		___, ___,        Key_Slash, ___,   ___,   ___
+	),
+	[SYMBOL] = KEYMAP_STACKED
 	(
 		___, ___, ___, ___, ___,
 		___, ___, ___, ___, ___,
